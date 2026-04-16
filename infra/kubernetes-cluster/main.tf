fc9e791057465dc202823ffa8db1f5ca817e077f
Ozgur Ozturk <ozgur@ozgurozturk.net>
2026-04-16 13:24:26 +0200

    feat(k8s): add complete Terraform files for k3s cluster provisioning

--- infra/kubernetes-cluster/main.tf
+++ infra/kubernetes-cluster/main.tf
@@ -35,13 +35,12 @@ resource "hcloud_firewall" "k8s" {
   }
 }
 
-# TODO: define the hcloud_server resource for the k3s cluster node
-# Use resource name "k8s_node" - outputs.tf depends on this name.
-# It should use:
-#   name         = "${var.environment}-k8s-node"
-#   image        = "ubuntu-24.04"
-#   server_type  = var.server_type
-#   location     = var.location
-#   ssh_keys     = [hcloud_ssh_key.k8s.id]
-#   firewall_ids = [hcloud_firewall.k8s.id]
-#   user_data    = file("${path.module}/cloud-init-k3s.yml")
+resource "hcloud_server" "k8s_node" {
+  name         = "${var.environment}-k8s-node"
+  image        = "ubuntu-24.04"
+  server_type  = var.server_type
+  location     = var.location
+  ssh_keys     = [hcloud_ssh_key.k8s.id]
+  firewall_ids = [hcloud_firewall.k8s.id]
+  user_data    = file("${path.module}/cloud-init-k3s.yml")
+}
